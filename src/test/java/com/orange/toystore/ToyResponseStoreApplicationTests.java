59b214cffc6ed9db258aef3ab655a578949869f0
hesham.ahmed <heshamahmed22226.ha@gmail.com>
2023-02-02 17:22:51 +0200

    impl integration test for updateToyEndpoint

--- src/test/java/com/orange/toystore/ToyResponseStoreApplicationTests.java
+++ src/test/java/com/orange/toystore/ToyResponseStoreApplicationTests.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.orange.toystore.api.Toy;
+import com.orange.toystore.api.ToyPutRequest;
 import com.orange.toystore.api.ToyResponse;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -15,11 +16,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -34,9 +32,7 @@ class ToyResponseStoreApplicationTests {
 	private String url;
 	@Autowired
 	private TestRepository repository;
-
 	private static ObjectWriter ow;
-
 
 	@BeforeAll
 	public static void init(){
@@ -44,8 +40,8 @@ class ToyResponseStoreApplicationTests {
 		objectMapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
 		ow = objectMapper.writer().withDefaultPrettyPrinter();
 	}
-
 	@Test
+	@Sql(statements = "DELETE FROM toys where id = 1", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 	public void testAddNewToyEndpoint() throws Exception {
 		Toy toy = new Toy();
 		toy.setAge(18);
@@ -63,12 +59,28 @@ class ToyResponseStoreApplicationTests {
 				.andExpect(content().json(ow.writeValueAsString(repository.findById(1L).get())))
 				.andReturn();
 	}
+	@Test
+	@Sql(statements = "INSERT INTO toys(id, age, color, description, name, price) VALUES (1, 18, 'color', 'desc', 'name', 20)"
+			, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "DELETE FROM toys where id = 1", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+	public void testUpdateToyEndpoint() throws Exception {
+		ToyPutRequest toy =
+				new ToyPutRequest(1L, "nameUpdated", "desc", 20, "color", 18);
+
+		mockMvc.perform(MockMvcRequestBuilders.put(url)
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(ow.writeValueAsString(toy)))
+				.andExpect(status().isAccepted())
+				.andExpect(content().json(ow.writeValueAsString(repository.findById(1L).get())))
+				.andReturn();
+
+	}
 
 	@Test
 	@Sql(statements = "INSERT INTO toys(id, age, color, description, name, price) VALUES (1, 18, 'color', 'desc', 'name', 20)"
 	, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 	@Sql(statements = "DELETE FROM toys where id = 1", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-	public void testListAllToys() throws Exception {
+	public void testListAllToysEndpoint() throws Exception {
 		List<ToyResponse> toys = new ArrayList<>();
 		ToyResponse toy = new ToyResponse(1L, "name", "desc", 20, "color", 18);
 		toys.add(toy);
@@ -79,5 +91,6 @@ class ToyResponseStoreApplicationTests {
 				.andReturn();
 
 	}
+
 
 }
